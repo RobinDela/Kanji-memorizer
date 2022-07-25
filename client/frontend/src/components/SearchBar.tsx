14cec4269731923095fed1cb57a06c5ebe7ba73f
RobinDela <78260403+RobinDela@users.noreply.github.com>
2022-07-25 16:18:09 +0900

    Fix type in searchbar component
    Remove useless useEffect

--- client/frontend/src/components/SearchBar.tsx
+++ client/frontend/src/components/SearchBar.tsx
@@ -1,13 +1,26 @@
-// @ts-nocheck
-// import { KanjiInformations } from './Types/KanjiApiData';
 import axios from "axios";
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import KanjiInformationsDisplay from "./KanjiInformationsDisplay";
 
 
+type KanjiInformations = {
+
+    kanji: string;
+    grade: number;
+    stroke_count: number;
+    meanings: string[];
+    kun_readings: string[];
+    on_readings: string[];
+    name_readings: string[];
+    jlpt: number;
+    unicode: string;
+    heisig_en: string;
+
+};
+
 const SearchBar = () => {
 
-    const [dataKanji, setDataKanji] = useState({
+    const [dataKanji, setDataKanji] = useState<KanjiInformations>({
 
         kanji: "",
         grade: 0,
@@ -21,35 +34,31 @@ const SearchBar = () => {
         heisig_en: ""
     });
 
-    const getKanji = async (x) => {
+    const getKanji = async (x: string) => {
         await axios(`https://kanjiapi.dev/v1/kanji/${x}`).then((response) => {
             setDataKanji(response.data);
             console.log(dataKanji)
         })
     };
     const [kanji, setKanji] = useState("");
-    const handleChange = (e) => {
+    const handleChange = (e: any) => {
         setKanji(e.target.value)
     }
     const handleClick = () => {
         getKanji(kanji)
     }
 
-
-    useEffect(() => {
-        getKanji();
-    }, [])
-
     return (
         <div className="App">
             <form onSubmit={handleClick}>
                 <input
+                    className="search-bar"
                     type="text"
                     placeholder="Search your kanji"
                     onChange={(e) => handleChange(e)}
                 />
             </form>
-            <button onClick={handleClick}>click</button>
+            <button type="button" onClick={handleClick}>click</button>
             <div>
                 <KanjiInformationsDisplay dataKanji={dataKanji} />
             </div>
